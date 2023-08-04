1374b1c2a201e65c484c6d4a06d2f239e2f7b1f5
Gavaskar <103166821+Gavaskar0543@users.noreply.github.com>
2023-08-04 03:14:02 +0000

    buttons added

--- calculator/src/Components/Calculator.js
+++ calculator/src/Components/Calculator.js
@@ -1,40 +1,82 @@
 import React from "react";
-import Styled from 'styled-components';
+import '../App.css';
 class Calculator extends React.Component{
 
 
     render(){
         return(
-           <OuterDiv>
-             <InnerDiv>
-                <Display>
-                    
-                </Display>
-                
-             </InnerDiv>
-           </OuterDiv>
+          <>
+           <div className="container w-50 border border-1">
+            <div className=" w-100 h-25 border border-3">
+              display
+            </div>
+            <div className="container">
+              
+            <div className="buttons-outer">
+            <div id="button" className="btn" data-value="AC">
+                <span>AC</span>
+            </div>
+            <div id="button" className="btn" data-value="Del">
+                <span>DEL</span>
+            </div>
+            <div id="button" className="btn" data-value="%">
+                <span>%</span>
+            </div>
+            <div id="button" className="btn" data-value="*">
+                <span>*</span>
+            </div>
+        <div id="button" className="btn"  data-value="9">
+            <span>9</span>
+        </div>
+        <div id="button" className="btn" data-value="8">
+            <span>8</span>
+        </div>
+        <div id="button" className="btn" data-value="7">
+            <span>7</span>
+        </div>
+        <div id="button" className="btn" data-value="/">
+            <span>/</span>
+        </div>
+        <div id="button" className="btn" data-value="6">
+            <span>6</span>
+        </div>
+        <div id="button" className="btn" data-value="5">
+            <span>5</span>
+        </div>
+        <div id="button" className="btn" data-value="4">
+            <span>4</span>
+        </div>
+        <div id="button" className="btn" data-value="-">
+            <span>-</span>
+        </div>
+        <div id="button" className="btn" data-value="3">
+            <span>3</span>
+        </div>
+        <div id="button" className="btn" data-value="2">
+            <span>2</span>
+        </div>
+        <div id="button" className="btn" data-value="1">
+            <span>1</span>
+        </div>
+        <div id="button" data-value="+">
+            <span>+</span>
+        </div>
+        <div id="button" className="btn zero"  data-value="0">
+            <span>0</span>
+        </div>
+        <div id="button" className="btn" data-value=".">
+            <span>.</span>
+        </div>
+        <div id="button" className="btn" data-value="=">
+            <span>=</span>
+        </div>
+        </div>
+            </div>
+           </div>
+          </>
+          
         )
     }
 }
-
-const OuterDiv = Styled.div`
-width:100vw;
-height:100vh;
-display:flex;
-
-font-family:verdana;
-`
-
-const InnerDiv = Styled.div`
-`
-
-const Display = Styled.div`
-height:5rem;
-width:25rem;
-border:2px solid black;
-
-`
-
-
-
 export default Calculator;
+
