8a8db031c7d68952ad9536efc992abd2e48fdb84
Gavaskar <gavaskaraeg@gmail.com>
2023-08-15 18:10:24 +0530

    styles added

--- calculator/src/Components/App.js
+++ calculator/src/Components/App.js
@@ -1,13 +1,60 @@
-
+import Styles from '../Styles/App.module.css'
 function App() {
   return (
    <>
-   <div>
-    <div>
+   <div className={Styles.calBody}>
+    <div className={Styles.disContainer}>
       display
     </div>
-    <div>
-      buttons
+    <div className={Styles.buttons}> 
+     <div>
+      1
+     </div>
+     <div>
+      2
+     </div>
+     <div>
+      3
+     </div>
+     <div>
+      4
+     </div>
+     <div>
+      5
+     </div>
+     <div>
+      6
+     </div>
+     <div>
+      7
+     </div>
+     <div>
+      8
+     </div>
+     <div>
+      9
+     </div>
+     <div>
+     0
+     </div>
+     <div>
+      *
+     </div>
+     <div>
+      /
+     </div>
+     <div>
+      +
+     </div>
+     <div>
+      -
+     </div>
+     <div>
+      =
+     </div>
+     <div>
+      Ac
+     </div>
     </div>
    </div>
    </>
