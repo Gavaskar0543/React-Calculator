62e8a641458d8443adf76fc6f294e75813aa7088
Gavaskar <gavaskaraeg@gmail.com>
2023-08-16 17:33:18 +0530

    only the multiplication part done

--- calculator/src/Components/App.js
+++ calculator/src/Components/App.js
@@ -4,25 +4,43 @@ function App() {
   const [operand1,setOperand1] = useState(0);
   const [operand2,setOperand2] = useState(0);
   const [operator,setOperator] = useState('');
-  const [display,setDisplay] = useState('');
+  const [display,setDisplay] = useState(0);
   
   /* The `useEffect` hook is used in React to perform side effects in functional components. In this
   case, the `useEffect` hook is being used to set the initial value of the `display` state to 10.
   The second argument `[]` is an empty dependency array, which means that the effect will only run
   once, when the component mounts. */
   useEffect(() => {
-    setDisplay();
+    setDisplay('');
+    
   },[]);
   const handleClearBtn = () =>{
     setDisplay('');
   }
   const handleNum = (e) => {
-   let incomeNums = '';
-    incomeNums = e.target.getAttribute('data-value')+incomeNums;
-   setDisplay(display+incomeNums);
+    const incomeNum = e.target.getAttribute('data-value');
+    setDisplay(display + incomeNum);
 
+    if (operator === '*') {
+      // Assuming multiplication logic
+      const result = parseFloat(display) * parseFloat(incomeNum);
+      setDisplay(result.toString());
+      setOperator(null); // Clear the operator after using it
+    }
    
-  }
+  };
+  const handleOperation = (value) => {
+    if (value === '*') {
+      setOperator(value);
+    }
+   
+     else if (value === '=') {
+      // Perform calculations here based on the operator
+      // For example: addition, subtraction, etc.
+      // Update the display with the result
+     
+    }
+  };
   return (
    <>
    <div className={Styles.calBody}>
@@ -54,7 +72,7 @@ function App() {
      <div>
       9
      </div>
-     <div className={Styles.orangeBtn}>
+     <div   className={Styles.orangeBtn} onClick={() => handleOperation('*')} >
       x
      </div>
      <div>
@@ -66,7 +84,7 @@ function App() {
      <div>
       6
      </div>
-     <div className={Styles.orangeBtn}>
+     <div className={Styles.orangeBtn} onClick={() => handleOperation('-')}>
       -
      </div>
      <div>
@@ -90,7 +108,7 @@ function App() {
      <div>
       .
      </div>
-     <div  className={Styles.orangeBtn}>
+     <div  className={Styles.orangeBtn}  onClick={() => handleOperation('=')}>
       =
      </div>
     </div>
