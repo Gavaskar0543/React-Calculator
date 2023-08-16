64a6af94a1fb49248845ee5b6f8244aa044ffabe
Gavaskar <gavaskaraeg@gmail.com>
2023-08-16 14:52:27 +0530

    stucture done

--- calculator/src/Components/App.js
+++ calculator/src/Components/App.js
@@ -1,12 +1,56 @@
+import React ,{useState} from 'react';
 import Styles from '../Styles/App.module.css'
 function App() {
+  const [operand1,setOperand1] = useState(0);
+  const [operand2,setOperand2] = useState(0);
+  const [operator,setOperator] = useState('');
+  const [display,setDisplay] = useState('');
   return (
    <>
    <div className={Styles.calBody}>
     <div className={Styles.disContainer}>
-      display
+     
+     <div>
+      456
+     </div>
     </div>
     <div className={Styles.buttons}> 
+     <div>
+      c
+     </div>
+     <div>
+      +/-
+     </div>
+     <div>
+      %
+     </div>
+     <div className={Styles.orangeBtn}>
+      /
+     </div>
+     <div>
+      7
+     </div>
+     <div>
+      9
+     </div>
+     <div>
+      7
+     </div>
+     <div className={Styles.orangeBtn}>
+      x
+     </div>
+     <div>
+      4
+     </div>
+     <div>
+     5
+     </div>
+     <div>
+      6
+     </div>
+     <div className={Styles.orangeBtn}>
+      -
+     </div>
      <div>
       1
      </div>
@@ -16,44 +60,20 @@ function App() {
      <div>
       3
      </div>
-     <div>
-      4
-     </div>
-     <div>
-      5
-     </div>
-     <div>
-      6
-     </div>
-     <div>
-      7
-     </div>
-     <div>
-      8
-     </div>
-     <div>
-      9
-     </div>
-     <div>
-     0
-     </div>
-     <div>
-      *
-     </div>
-     <div>
-      /
-     </div>
-     <div>
+     <div className={Styles.orangeBtn}>
       +
      </div>
      <div>
-      -
+      0
      </div>
      <div>
+      ....
+     </div>
+     <div>
+      .
+     </div>
+     <div  className={Styles.orangeBtn}>
       =
-     </div>
-     <div>
-      Ac
      </div>
     </div>
    </div>
